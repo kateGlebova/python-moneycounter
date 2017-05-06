60a9c991f60f994276e1d30d4d975eb6246be606
User <victorvladimirovichkravhcuk@gmail.com>
2017-05-07 00:36:25 +0300

    added counter class

---
+++ counter.py
@@ -0,0 +1,46 @@
+class Counter:
+
+    def __init__(self):
+        self.operations_list = []
+
+    def load_from_file(self, path):
+        import pickle
+        f = open(path, 'rb')
+        self.operations_list = pickle.load(f)
+        f.close()
+
+    def add_operation(self, operation):
+        self.operations_list.append(operation)
+
+    def get_operations_by_date(self, date):
+        res = list()
+        for k in self.operations_list:
+            if k.date == date:
+                res.append(k)
+        return res
+
+    def get_operations_by_description(self, description):
+        res = list()
+        for k in self.operations_list:
+            if k.description == description:
+                res.append(k)
+        return res
+
+    def get_operations_by_money(self, money):
+        res = list()
+        for k in self.operations_list:
+            if k.money == money:
+                res.append(k)
+        return res
+
+    def set_operations(self, operations_list):
+        self.operations_list = operations_list
+
+    def get_operations(self):
+        return self.operations_list
+
+    def get_balance(self):
+        balance = 0
+        for k in self.operations_list:
+            balance = balance + k.get_money()
+        return balance
