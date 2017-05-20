07dfa7ee5751d104f9fff2c4f6c977b1242297ea
User <victorvladimirovichkravhcuk@gmail.com>
2017-05-20 11:45:25 +0300

    fix

--- storage_pickle.py
+++ storage_pickle.py
@@ -6,7 +6,7 @@ class PickleFormat:
     @staticmethod
     def serialization(file, data):
         with file:
-            file.write(pickle.dumps(file))
+            file.write(pickle.dumps(data))
         return data
 
     @staticmethod
